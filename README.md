ef87aed15372890acfc8cb449592623cd00d25e5
ValerioL94 <valerio.lemma1994@gmail.com>
2024-01-26 13:15:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# webpack-template
+
+Template for webpack
+
+-To start run:
+npm install
+
+-Setup github pages:
+git add dist && git commit -m "Initial dist subtree commit"
+
+-Useful dependencies:
+npm install date-fns --save
---
+++ webpack.common.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            favicon: '',
+            template: './src/template.html',
+        }),
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: 'images/[name][ext][query]',
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
